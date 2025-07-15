2825ef00a5b29055fcfa3832892307962c7497f4
brunoramossilva <brunogabriel0900@gmail.com>
2025-07-15 11:43:35 -0300

    changes on structure

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,3 @@
 import { PrismaClient } from "../generated/prisma/index.js"
 
-const prisma = new PrismaClient()
-
-async function getProducts() {
-    const product = await prisma.product.findMany()
-    console.log(product)
-}
-
-getProducts()
+export const prisma = new PrismaClient()
--- src/server.js
+++ src/server.js
@@ -1,13 +1,39 @@
 import app from "./app.js";
+import { prisma } from "./database/index.js";
 
 app.get("/", (req, res) => {
     res.json("Bem-vindo à Lojinha Social!");
 })
 
-app.get("/produto", (req, res) => {
-    res.json("Boneca de pano.");
-});
+
+app.get("/produto", async (req, res) => {
+    try {
+        const products = await prisma.product.findMany();
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+app.get("/:produtoId", async (req, res) => {
+    const produtoId = req.params.produtoId
+
+    const produto = await prisma.product.findFirst({
+        where: { id: produtoId }
+    })
+    res.json(produto);
+})
+
+
+app.post("/publicarProduto", (req, res) => {
+    const data = req.body
+
+    console.log(data)
+    res.json(data)
+})
+
 
 app.listen(3001, () => {
-    console.log("Servidor rodando na porta 3001, acesse em http://localhost:3001/produto");
+    console.log("Servidor rodando na porta 3001, acesse em http://localhost:3001");
 });
