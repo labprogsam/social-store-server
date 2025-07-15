7bab40c44e7d915baece0f1b457e14a6daa3c3d9
brunoramossilva <brunogabriel0900@gmail.com>
2025-07-15 12:48:49 -0300

    crud completo no index

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+// .env
+// npx prisma
+// npx prisma studio
+
 import app from "./app.js";
 import { prisma } from "./database/index.js";
 
@@ -26,13 +30,37 @@ app.get("/:produtoId", async (req, res) => {
 })
 
 
-app.post("/publicarProduto", (req, res) => {
-    const data = req.body
+app.post("/publicarProduto", async (req, res) => {
+    const dadosCorpo = req.body
 
-    console.log(data)
-    res.json(data)
+    const novoProduto = await prisma.product.create({
+        data: dadosCorpo
+    })
+
+    res.json(novoProduto)
 })
 
+
+app.put("/:produtoId", async (req, res) => {
+    const produtoId = req.params.produtoId
+    const dadosCorpo = req.body // Depois adicionar validações com return e destruturar (melhor)
+
+    const produtoEditado = await prisma.product.update({
+        where: { id: produtoId },
+        data: dadosCorpo
+    })
+
+    res.json(produtoEditado)
+})
+
+
+app.delete("/:produtoId", async (req, res) => {
+    const productId = req.params.produtoId
+    const produtoDeletetado = await prisma.product.delete({
+        where: { id: productId }
+    })
+    res.json(produtoDeletetado)
+})
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001, acesse em http://localhost:3001");
