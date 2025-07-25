0c7d569eabdfd7d1af1f30ec3f08ca8cb21989ab
labprogsam <som3@cin.ufpe.br>
2025-07-24 21:21:43 -0300

    chore: remove comments

--- src/repositories/productRepository.js
+++ src/repositories/productRepository.js
@@ -1,11 +1,7 @@
-// Aqui importamos o PrismaClient do diretório gerado pelo Prisma
 import { prisma } from "../database/index.js";
 
-// Exportamos um repositório de produtos que contém métodos para interagir com o banco de dados
-// Isso permite que possamos realizar operações CRUD nos produtos
 export const ProductRepository = {
-  // Métodos para encontrar, criar, atualizar e deletar produtos
-  // Cada método utiliza o PrismaClient para realizar operações no banco de dados
+
   findMany: (params = {}) => {
     return prisma.product.findMany(params);
   },
@@ -54,8 +50,6 @@ export const ProductRepository = {
     })
   },
 
-  // Método para encontrar um produto por ID
-  // Utiliza o método findUnique do PrismaClient para buscar um produto específico
   findUnique: (id) => {
     return prisma.product.findUnique({
       where: { id },
@@ -65,14 +59,10 @@ export const ProductRepository = {
     });
   },
 
-  // Método para criar um novo produto
-  // Recebe um objeto de dados do produto e utiliza o método create do PrismaClient
   create: (data) => {
     return prisma.product.create({ data });
   },
 
-  // Métodos para atualizar e deletar produtos
-  // O método recebe um ID e os dados a serem atualizados
   update: (id, data) => {
     return prisma.product.update({
       where: { id },
@@ -88,7 +78,6 @@ export const ProductRepository = {
     });
   },
 
-  // O método delete recebe um ID e utiliza o método delete do PrismaClient
   delete: (id) => {
     return prisma.product.delete({
       where: { id },
